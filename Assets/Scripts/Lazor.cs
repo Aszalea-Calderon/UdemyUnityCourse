2a89b5ebb2a2f0d04dc5574c03379eb4a2deadd9
Aszalea-Calderon <aszalea_calderon@yahoo.com>
2022-04-25 08:22:24 -0400

    cleanup code

--- Assets/Scripts/Lazor.cs
+++ Assets/Scripts/Lazor.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Lazor : MonoBehaviour
 {
     [SerializeField]
+    //Lazor Speed
     private float _laserSpeed = 10f;
 
     // Start is called before the first frame update
@@ -16,9 +17,10 @@ public class Lazor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Lazor Speed
         transform.Translate(Vector3.up * _laserSpeed * Time.deltaTime);
-        //when leaving if lazor at 7 y 
-        //destory object
+
+        //Destroy lazor if out of window
         if (transform.position.y > 7f)
         {
             Destroy(gameObject);
