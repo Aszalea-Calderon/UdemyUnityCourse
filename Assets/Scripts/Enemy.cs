f037648b993c28a0704fc92f885ca2d3ca54fe84
Aszalea-Calderon <aszalea_calderon@yahoo.com>
2022-04-26 08:53:24 -0400

    added random number generator to enemy and used said number to teleport to new locaiton

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -16,17 +16,24 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // TeleportToTop();
+        TeleportToTop();
         //Enemy Speed
         transform.Translate(Vector3.down * _enemySpeed * Time.deltaTime);
 
 
 
-        // void TeleportToTop(){
-        //     if (transform.position.y < -5f ){
-        //         transform.position = new Vector3()
-        //     }
-        // }
+        void TeleportToTop()
+        {
+            if (transform.position.y < -5f)
+            {
+                int xCount = Random.Range(-10, 10);
+                float yCount = 7.6f;
+                transform.position = new Vector3(xCount, yCount, 0);
+                //create random nums for x and y
+                // set teleport on bottom to use xCount 7.6 and 0 for y
+                //max is 10 x and min is -10
+            }
+        }
     }
 }
 // move down at 4 meters per second
