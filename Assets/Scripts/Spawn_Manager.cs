caeba17ff9db024404930ba648a2ef85461c9ff4
Aszalea-Calderon <aszalea_calderon@yahoo.com>
2022-04-30 09:24:14 -0400

    Ienumerator

---
+++ Assets/Scripts/Spawn_Manager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // IEnumerator SpawnRoutine(){
+
+    // }
+    //Spawn game objects every 5 seconds
+    //Create a coroutine of type IEnumerator -- Yield Events
+    //While loop
+}
