ed1267fe766b9524f1321de7df9184fdd15aef84
Aszalea-Calderon <aszalea_calderon@yahoo.com>
2022-05-04 09:17:46 -0400

    spawn manager

--- Assets/Scripts/Spawn_Manager.cs
+++ Assets/Scripts/Spawn_Manager.cs
@@ -16,10 +16,20 @@ public class Spawn_Manager : MonoBehaviour
 
     }
 
-    // IEnumerator SpawnRoutine(){
+    //Create timer that counts down to 5 
+    //at 0 set off SpawnRoutine
+    //
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
 
-    // }
+        }
+        //   While loop (infinite loop)
+        //   Instantiate enemy prefeab
+        //   Yield wait for 5 seconds 
+    }
     //Spawn game objects every 5 seconds
     //Create a coroutine of type IEnumerator -- Yield Events
-    //While loop
+
 }
